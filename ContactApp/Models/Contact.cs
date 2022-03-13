40a9c1d2712e80142571d277d6c5801ff24f1ff3
Mohammed Tausif Uddin Ansari <mohammed.tausifansari@gmail.com>
2022-03-13 16:00:04 +0530

    first commit

---
+++ ContactApp/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactApp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Number { get; set; }
+        public string Email { get; set; }
+    }
+}
